e0e7a953a790c0ebc42198217a32b42040ac9e7c
shivaapratim <shuklashivang9@gmail.com>
2025-08-01 02:36:57 +0530

    commi4

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
+const API_URL =  'https://ai-component-backend.onrender.com';
 
 const Register = () => {
     const [email, setEmail] = useState('');
